75ff82fe21b6634350de17a2aa1d8aa68e2e78cd
MrOttimista <40366999+MrOttimista@users.noreply.github.com>
2020-06-22 16:48:32 +0200

    s

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose')
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://127.0.0.1/weTube'
 const methodOverride = require('method-override')
 const uploadRouter = require('./routes/uploadRouter')
 const videosRouter = require('./routes/videosRouter')
 app.use(express.static( "moduleVideo" ));
 app.set('view engine' , 'ejs');
 const PORT = process.env.PORT || 3000
-mongoose.connect('mongodb://127.0.0.1/weTube' ,  { useNewUrlParser: true , useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URI ,  { useNewUrlParser: true , useUnifiedTopology: true })
 app.use(express.urlencoded({extended : false}))
 app.use('/upload' , uploadRouter);
 app.use('/videos' , videosRouter)
